0845886d0ce923b961aa5eb0e343446bace6a052
ybtu <ybtu@cfda4.citi.sinica.edu.tw>
2024-01-16 14:40:08 +0800

    update path that links to 10-Q collections and indexes

--- config.py
+++ config.py
@@ -2,5 +2,11 @@
 
 ROOT = "/home/ybtu/FinNLP"
 RAW_DIR = "/home/ybtu/FinNLP/Jsonl_Data"
-FORMMATED_DIR = "/home/ybtu/FinNLP/collections/basic"
-INDEX_DIR = "/home/ybtu/FinNLP/indexes"
+
+# for 10-Q
+FORMMATED_DIR = "/home/ybtu/FinNLP/collections/10Q/basic"
+INDEX_DIR = "/home/ybtu/FinNLP/indexes/10Q"
+
+# for 10-K
+# FORMMATED_DIR = "/home/ybtu/FinNLP/collections/10K/basic"
+# INDEX_DIR = "/home/ybtu/FinNLP/indexes/10K"
