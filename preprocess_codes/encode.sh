707049b6bd260c93bb4c08c47bd687b7166c1f29
ybtu <ybtu@cfda4.citi.sinica.edu.tw>
2024-01-17 22:54:17 +0800

    take arguments

--- preprocess_codes/encode.sh
+++ preprocess_codes/encode.sh
@@ -1,11 +1,15 @@
 # /bin/bash
 
+FORM=$1                 # 10Q or 10K
+SOURCE=$2               # basic, meta_data, title
+BATCH_SIZE=${3:-32}
+
 python -m pyserini.encode \
-  input   --corpus ../collections/basic \
+  input   --corpus ../collections/${FORM}/${SOURCE} \
           --shard-id 0 \
           --shard-num 1 \
-  output  --embeddings ../indexes/dpr-ctx_encoder-multiset-base \
+  output  --embeddings ../indexes/${FORM}/dpr-ctx_encoder-multiset-base-${SOURCE} \
           --to-faiss \
   encoder --encoder facebook/dpr-ctx_encoder-multiset-base \
-          --batch 32 \
+          --batch ${BATCH_SIZE} \
           --fp16  # if inference with autocast()
