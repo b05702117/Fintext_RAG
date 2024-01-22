0f394dedc9c84d97f55cd9cca7c8192fe990fb2d
ybtu <ybtu@cfda4.citi.sinica.edu.tw>
2024-01-22 10:41:57 +0800

    switch dir to 10K

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ ROOT = "/home/ybtu/FinNLP"
 RAW_DIR = "/home/ybtu/FinNLP/Jsonl_Data"
 
 # for 10-Q
-FORMMATED_DIR = "/home/ybtu/FinNLP/collections/10Q/basic"
-INDEX_DIR = "/home/ybtu/FinNLP/indexes/10Q"
+# FORMMATED_DIR = "/home/ybtu/FinNLP/collections/10Q/basic"
+# INDEX_DIR = "/home/ybtu/FinNLP/indexes/10Q"
 
 # for 10-K
-# FORMMATED_DIR = "/home/ybtu/FinNLP/collections/10K/basic"
-# INDEX_DIR = "/home/ybtu/FinNLP/indexes/10K"
+FORMMATED_DIR = "/home/ybtu/FinNLP/collections/10K/basic"
+INDEX_DIR = "/home/ybtu/FinNLP/indexes/10K"
