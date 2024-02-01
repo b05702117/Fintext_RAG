33854c5662a85032a98217d128adbc8a051d86dc
AndyYTHsiao <yyddandy@gmail.com>
2024-02-01 10:02:11 +0800

    Use f-string to reduce errors

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # config.py
 
 ROOT = "/home/ybtu/FinNLP"
-RAW_DIR = "/home/ybtu/FinNLP/Jsonl_Data"
+RAW_DIR = f"{ROOT}/Jsonl_Data"
 
 # for 10-Q
-FORMMATED_DIR = "/home/ybtu/FinNLP/collections/10Q/basic"
-INDEX_DIR = "/home/ybtu/FinNLP/indexes/10Q"
+# FORMMATED_DIR = f"{ROOT}/collections/10Q/basic"
+# INDEX_DIR = f"{ROOT}/indexes/10Q"
 
 # for 10-K
-# FORMMATED_DIR = "/home/ybtu/FinNLP/collections/10K/basic"
-# INDEX_DIR = "/home/ybtu/FinNLP/indexes/10K"
+FORMMATED_DIR = f"{ROOT}/collections/10K/basic"
+INDEX_DIR = f"{ROOT}/indexes/10K"
